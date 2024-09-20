8d982be9fe73afd32fea50a35aa14661fe20e154
AzaelAD <alazamdel@gmail.com>
2024-09-20 09:38:46 -0700

    Ya todo

--- controllers/proyectController.js
+++ controllers/proyectController.js
@@ -3,15 +3,35 @@ const { v4: uuidv4 } = require('uuid');
 
 let proyects = [
     {
-        id: uuidv4(),
+        id: '5f0dba4a-e8d3-4a63-9cf1-741c53f6be72',
         name: "Nuevo Sistema de Gestion",
         description: "Implementar un sistema de recursos",
         startDate: "2024-09-01",
-        endDate: "2025-02-01",
+        endDate: "2025-02-01"   ,
         status: "ongoing",
         teamMembers: ["Jaime Torres", "Aldo Amaral", "Karina Macedo"],
         budget: 10000
-    }
+    },
+    {
+        id: uuidv4(),
+        name: "Nuevo Sistema de Gestion Más nuevo",
+        description: "Implementar un sistema de recursos",
+        startDate: "2024-09-01",
+        endDate: "2026-02-01"   ,
+        status: "ongoing",
+        teamMembers: ["Jaime Torres", "Aldo Amaral", "Karina Macedo"],
+        budget: 10000
+    },
+    {
+        id: uuidv4(),
+        name: "Si",
+        description: "Implementar un sistema de recursos",
+        startDate: "2024-09-01",
+        endDate: "2035-02-01"   ,
+        status: "ongoing",
+        teamMembers: ["Jaime Torres", "Aldo Amaral", "Karina Macedo"],
+        budget: 10000
+    },
 ]
 
 function getAllProyects() {
